609ec7f69bdc3555afd30a23cd85083d934f1fc8
Nelson <43588033+NelsonManuelGM@users.noreply.github.com>
2021-10-25 17:33:44 -0400

    configuraiton for database

--- src/configuration/configuration.module.ts
+++ src/configuration/configuration.module.ts
@@ -2,18 +2,17 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
-//TODO add mongoose configuration
-//TODO add Command configuration
 @Module({
   imports: [
     ConfigModule.forRoot({}),
     MongooseModule.forRootAsync({
       useFactory: () => ({
+        uri: `mongodb://${process.env.DB_HOST}`,
         dbName: process.env.DB_NAME,
         user: process.env.DB_USER,
         pass: process.env.DB_PASSWORD,
       })
-    })
+    }),
   ],
   controllers: [],
   providers: [],
