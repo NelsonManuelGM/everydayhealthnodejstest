8cb8a55fca94f35e5ba55969ae0f75c753abc4f1
Nelson <43588033+NelsonManuelGM@users.noreply.github.com>
2021-10-26 18:54:32 -0400

    adding e2e test

--- test/tracking-data.controller.e2e-spec.ts
+++ test/tracking-data.controller.e2e-spec.ts
@@ -6,20 +6,35 @@ import { TrackingDataService } from '../src/tracking-data/services/tracking-data
 
 describe('TrackingDataController', () => {
   let app: INestApplication;
-  const data = id => ({
+  const summary = [
+    {
+      "_id": "2021-06-01",
+      "count": 3
+    },
+    {
+      "_id": "2021-06-02",
+      "count": 6
+    }]
+
+  const nLSummaryData = id => ({
     "newsletter_id": id,
-    "summary": [
-      {
-        "_id": "2021-06-01",
-        "count": 3
-      },
-      {
-        "_id": "2021-06-02",
-        "count": 6
-      }]
+    "summary": summary
   })
+  const userSummaryData = id => ({
+    "user_id": id,
+    "summary": summary
+  })
+
+  const nlActionSummaryData = id => ({
+    "newsletter_id": id,
+    "action": "open",
+    "summary": summary
+  })
+
   const mockTrackingDataService = {
-    getNLSummary: jest.fn((id) => data(id))
+    getNLSummary: jest.fn((id) => nLSummaryData(id)),
+    getUserSummary: jest.fn((id) => userSummaryData(id)),
+    getNLActionSummary: jest.fn((id) => nlActionSummaryData(id)),
   }
 
   beforeEach(async () => {
@@ -37,11 +52,29 @@ describe('TrackingDataController', () => {
 
 
   it('GET nlsummary/:id', async () => {
-    const newsletter_id = 101
+    const newsletter_id = 101;
 
     return request(app.getHttpServer())
       .get(`/tracking-data/nlsummary/${newsletter_id}`)
       .expect(200)
-      .expect(data(newsletter_id));
+      .expect(nLSummaryData(newsletter_id));
+  });
+
+  it('GET usersummary/:id', async () => {
+    const user_id = 6;
+
+    return request(app.getHttpServer())
+      .get(`/tracking-data/usersummary/${user_id}`)
+      .expect(200)
+      .expect(userSummaryData(user_id));
+  });
+
+  it('GET nlactionsummary/:id', async () => {
+    const newsletter_id = 101;
+
+    return request(app.getHttpServer())
+      .get(`/tracking-data/nlactionsummary/${newsletter_id}`)
+      .expect(200)
+      .expect(nlActionSummaryData(newsletter_id));
   });
 });
